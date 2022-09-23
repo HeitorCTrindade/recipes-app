8bedb2deeac02f849f0669669f1e52745ffaecc3
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-09-23 19:28:20 -0300

    Requisito 2, 3, 4, 5 e 6 - faltam os testes, 45% e 90%

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,91 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {
+  DRINKS_TOKEN_KEY,
+  MEALS_TOKEN_KEY,
+  saveLocalStorageItem,
+  USER_KEY,
+} from '../services/localStorageFuncs';
 
-export default function Login() {
+const MIN_PASSWORD_LENGTH = 7;
+function Login(props) {
+  const INITIAL_STATE = {
+    email: '',
+    password: '',
+  };
+  const [user, setUser] = useState(INITIAL_STATE);
+  const [isLoginButtonDisabled, setIsLoginButtonDisabled] = useState(true);
+
+  const isValidEmail = (inputEmail) => String(inputEmail)
+    .toLowerCase()
+    .match(
+      /^[^ ]+@[^ ]+\.[a-z]{2,3}$/,
+    );
+
+  const handleChanges = ({ target }) => {
+    const { name, value } = target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
+
+  const handleLoginClickButton = () => {
+    const { history } = props;
+    saveLocalStorageItem(USER_KEY, user.email);
+    saveLocalStorageItem(DRINKS_TOKEN_KEY, 1);
+    saveLocalStorageItem(MEALS_TOKEN_KEY, 1);
+    history.push('/meals');
+  };
+
+  useEffect(() => {
+    const verifyLoginRequest = () => {
+      const { email, password } = user;
+      if (isValidEmail(email) && password.length >= MIN_PASSWORD_LENGTH) {
+        setIsLoginButtonDisabled(false);
+      } else {
+        setIsLoginButtonDisabled(true);
+      }
+    };
+    verifyLoginRequest();
+  }, [user]);
+
   return (
-    <div>Login</div>
+    <main>
+      <input
+        type="email"
+        name="email"
+        value={ user.email }
+        data-testid="email-input"
+        onChange={ handleChanges }
+      />
+      <input
+        type="password"
+        name="password"
+        value={ user.password }
+        data-testid="password-input"
+        onChange={ handleChanges }
+      />
+      <button
+        type="button"
+        disabled={ isLoginButtonDisabled }
+        data-testid="login-submit-btn"
+        onClick={ handleLoginClickButton }
+      >
+        Entrar
+      </button>
+    </main>
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  loginButton: (state) => dispatch(submitLogin(state)) });
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
