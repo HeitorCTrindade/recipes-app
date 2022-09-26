0d5721ce4d715288e0c3aa49ebf2f8ac6a418cc5
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-09-26 17:32:29 -0300

    Footer Component e seus testes implementados. Requisitos 16, 17 e 18 finalizados.

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Footer from '../components/Footer';
 
-export default function Meals() {
+export default function Meals(props) {
+  const { history } = props;
   return (
-    <div>Meals</div>
+    <div>
+      Meals
+      <Footer history={ history } />
+    </div>
   );
 }
+
+Meals.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
