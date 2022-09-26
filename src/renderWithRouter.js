14a4273d4c0ffba42d32a663f8cde28477a649f7
Geomarcia Ramos <geoframos@gmail.com>
2022-09-26 20:03:18 -0300

    teste de cobertura da tela de login

---
+++ src/renderWithRouter.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={ history }>{component}</Router>),
+    history,
+  });
+};
+
+export default renderWithRouter;
