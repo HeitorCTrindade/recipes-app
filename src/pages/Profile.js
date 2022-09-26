79900f05e5785aca0f3f288752ed274124c2be6b
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-09-26 20:25:40 -0300

    Profile Componet implementado - Req 56~62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import {
+  readLocalStorage,
+  USER_KEY,
+  clearLocalStorage,
+} from '../services/localStorageFuncs';
 
 export default function Profile(props) {
   const { history } = props;
+  const [userEmail, setEmail] = useState('');
+
+  useEffect(() => {
+    const { email } = readLocalStorage(USER_KEY);
+    setEmail(email);
+  }, []);
+
   return (
     <div>
-      <Header
-        title="Profile"
-        history={ history }
-      />
-      Profile
+      <Header title="Profile" history={ history } />
+      <h1 data-testid="profile-email">{userEmail}</h1>
+      <button
+        type="button"
+        onClick={ () => {
+          history.push('/done-recipes');
+        } }
+        className="btn btn-warning"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        onClick={ () => {
+          history.push('/favorite-recipes');
+        } }
+        className="btn btn-warning"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+      </button>
+
+      <button
+        type="button"
+        onClick={ () => {
+          clearLocalStorage();
+          history.push('/');
+        } }
+        className="btn btn-warning"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+      </button>
       <Footer history={ history } />
     </div>
   );
