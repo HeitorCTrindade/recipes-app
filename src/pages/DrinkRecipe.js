fe2777761e70f7bba4af7acff381adaee83c2ba3
Hugo Leonardo <huleonardo19@gmail.com>
2022-09-27 01:01:35 -0300

    req 24: apis chamadas

--- src/pages/DrinkRecipe.js
+++ src/pages/DrinkRecipe.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { fetchDrinksDetail } from '../services/fetchDetails';
 
 export default function DrinkRecipe() {
+  const { id } = useParams();
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+    const recipeDetails = async () => {
+      const response = await fetchDrinksDetail(id);
+      setDetails(...response);
+    };
+    recipeDetails();
+  }, [id]);
+
+  console.log(details);
   return (
-    <div>DrinkRecipe</div>
+    <div>
+      MealRecipe
+      {' '}
+      {id}
+    </div>
   );
 }
--- src/pages/MealRecipe.js
+++ src/pages/MealRecipe.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { fetchMealDetail } from '../services/fetchDetails';
 
 export default function MealRecipe() {
+  const { id } = useParams();
+  const [details, setDetails] = useState([]);
+
+  useEffect(() => {
+    const recipeDetails = async () => {
+      const response = await fetchMealDetail(id);
+      setDetails(...response);
+    };
+    recipeDetails();
+  }, [id]);
+
+  console.log(details);
   return (
-    <div>MealRecipe</div>
+    <div>
+      MealRecipe
+      {' '}
+      {id}
+    </div>
   );
 }
